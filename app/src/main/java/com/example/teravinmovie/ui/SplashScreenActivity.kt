290a507351662ad85cea00c587183c6309800676
yusufmr666 <yusufrohmatulloh666@gmail.com>
2023-10-12 00:07:01 +0700

    Initial Commit

--- app/src/main/java/com/example/teravinmovie/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/teravinmovie/ui/SplashScreenActivity.kt
@@ -1,12 +1,16 @@
 package com.example.teravinmovie.ui
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.teravinmovie.R
+
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
@@ -16,6 +20,9 @@ class SplashScreenActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        if ((this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo == null) {
+            Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show()
+        }
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
